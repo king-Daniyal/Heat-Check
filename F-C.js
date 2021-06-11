23e8928dc4cb4627e4281514933b7502209fd871
Danoxzilla <67100321+king-Daniyal@users.noreply.github.com>
2021-06-11 13:25:58 +0400

    Create F-C.js

---
+++ F-C.js
@@ -0,0 +1,9 @@
+// program to convert celsius to fahrenheit
+// ask the celsius value to the user 
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate Celsius
+celsius = (fahrenheit - 32) / 1.8
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
